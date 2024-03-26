3d431f0deb048b3359fad9ffc72df8b2bbc34bb7
Анастасия Пуха <pukha_an@mail.ru>
2024-03-26 23:30:42 +0300

    created project

---
+++ src/main/java/com/application/ptsdwebapplication/PtsdWebApplication.java
@@ -0,0 +1,13 @@
+package com.application.ptsdwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtsdWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtsdWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/ptsdwebapplication/PtsdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.ptsdwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtsdWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
